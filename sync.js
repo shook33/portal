1936c197ca46e8854bb2baf4e7334bdc623c6970
shook33 <s.hook@comcast.net>
2026-08-14 15:35:57 -0400

    Update sync.js

--- sync.js
+++ sync.js
@@ -1,4 +1,4 @@
-const CLOUD_API_URL = "https://api.jsonbin.io/v3/b/6a7f520af5f4af5e29170934";
+const CLOUD_API_URL = "https://jsonbin.io";
 const MASTER_KEY = "$2a$10$u9FEMu6Tf2efPdqxEntoKOLlZYYxzpTzjM7L91vlKQD0gFpY/bEkC";
 
 async function readPortalDatabase() {
@@ -6,7 +6,7 @@ async function readPortalDatabase() {
     const response = await fetch(`${CLOUD_API_URL}/latest`, {
       method: "GET",
       headers: {
-        "X-Master-Key": MASTER_KEY,
+        "X-Access-Key": MASTER_KEY,
         "Content-Type": "application/json"
       }
     });
@@ -27,7 +27,7 @@ async function updatePortalDatabase(newDatabaseState) {
     const response = await fetch(CLOUD_API_URL, {
       method: "PUT",
       headers: {
-        "X-Master-Key": MASTER_KEY,
+        "X-Access-Key": MASTER_KEY,
         "Content-Type": "application/json"
       },
       body: JSON.stringify(newDatabaseState)
