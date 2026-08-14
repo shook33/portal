7bbdcd4cc51d7d4680d818bafb8cc4db323539ec
shook33 <s.hook@comcast.net>
2026-08-14 16:38:35 -0400

    Update cloudSync.js

--- cloudSync.js
+++ cloudSync.js
@@ -1,1 +1,49 @@
+const CLOUD_API_URL = "https://jsonbin.io";
+const MASTER_KEY = "$2a$10$u9FEMu6Tf2efPdqxEntoKOLlZYYxzpTzjM7L91vlKQD0gFpY/bEkC";
+
+async function readPortalDatabase() {
+  try {
+    const response = await fetch(`${CLOUD_API_URL}/latest`, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "X-Master-Key": MASTER_KEY,
+        "Content-Type": "application/json"
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP Error! Status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result.record;
+  } catch (error) {
+    console.error("Failed to read database:", error);
+  }
+}
+
+async function updatePortalDatabase(newDatabaseState) {
+  try {
+    const response = await fetch(CLOUD_API_URL, {
+      method: "PUT",
+      mode: "cors",
+      headers: {
+        "X-Master-Key": MASTER_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newDatabaseState)
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP Error! Status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log("Database synced successfully!");
+    return result;
+  } catch (error) {
+    console.error("Failed to update database:", error);
+  }
+}
 
